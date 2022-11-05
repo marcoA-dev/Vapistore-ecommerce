5a942fd6a41791de1efa6ea3ac34e371bfa13ce1
marcoA-dev <marcoignacio.ac@gmail.com>
2022-11-04 23:53:26 -0300

    PreEntrega1+Aviles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import NavBar from "./Components/NavBar";
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar></NavBar>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,33 @@
+
+const NavBar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="index">
+                        <img src="assets/vapistorelogo.png" alt="" width="50" height="50"></img>
+                        Vapistore
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="index.html">Catalogo</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="features.html">Contacto</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="pricing.html">Sobre Nosotros</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
