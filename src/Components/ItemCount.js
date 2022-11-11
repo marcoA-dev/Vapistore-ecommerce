c683cc142433aadceee322c0ed54f21cb15e1767
marcoA-dev <marcoignacio.ac@gmail.com>
2022-11-11 19:34:06 -0300

    Contador con boton Actividad N°1

---
+++ src/Components/ItemCount.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useState ,useEffect } from 'react';
+import {BiPlus} from "react-icons/bi";
+import {BiMinus} from "react-icons/bi";
+import "../Styles/ItemCount.css";
+
+const ItemCount = ({initial,stock,onAdd}) => {
+
+  const [counter , setCounter] = useState(parseInt(initial));
+
+
+
+  const Decrease = () =>{
+   setCounter(counter -1)
+  }
+
+  const Increase = () =>{
+    setCounter(counter + 1);
+  }
+  
+  useEffect(() => {
+    setCounter(parseInt(initial));
+  },[initial])
+  
+
+ return (
+    <>
+    <div className="btn-group" role="group" aria-label="Basic example">
+        <button 
+        type="button" 
+        className="btn btn-primary Decrease" 
+        onClick={Decrease}
+        disabled={counter <=0}
+        id='decrease'>
+            <BiMinus></BiMinus>
+        </button>
+        <span 
+        type="number" 
+        className='Counter'
+        >
+          {counter}
+        </span>
+        <button 
+        type="button" 
+        className="btn btn-primary Increase" 
+        onClick={Increase}
+        disabled= {counter >= stock}>
+            <BiPlus></BiPlus></button>
+        <button
+        onClick={()=> onAdd(counter)} 
+        className='AgregarCarrito'>
+            Agregar al Carrito
+        </button>
+    </div>
+    </>
+  )
+}
+
+export default ItemCount
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,8 +1,24 @@
+/* eslint-disable no-template-curly-in-string */
 import React from 'react'
+import ItemCount from './ItemCount'
+import Swal from 'sweetalert2';
 
 const ItemListContainer = ({greeting}) => {
+
+  const onAdd = (quantity) =>{
+    Swal.fire(
+      'Good job!',
+      `Se Agregaron exitosamente ${quantity} unidades al Carrito`,
+      'success'
+    )
+  }
+
   return (
-    <div>{greeting}</div>
+    <>
+    {greeting}
+    <ItemCount initial ={1} stock={5} onAdd={onAdd}></ItemCount>
+    </>
+    
   )
 }
 
